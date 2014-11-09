82ddac4e6f710635afb01a84bcb91c0b01f310ae
Daniel <danielhernandez16@hotmail.com>
2014-11-09 16:25:15 -0500

    Configuracion Basica para separar a Express del server

---
+++ app/expressServer.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var ExpressServer = function(config){
+	config = config || {}; 
+
+	this.expressServer = express();
+
+	this.expressServer.get('/article/save/', function(req,res,next){
+	    res.send('Hello from article save');
+	});
+
+	this.expressServer.get('/article/remove/', function(req,res,next){
+	    res.send('Hello from article remove');
+	});
+
+	this.expressServer.get('/article/add/', function(req,res,next){
+	    res.send('Hello from article add');
+	});
+
+	this.expressServer.get('/article/see/:data', function(req,res,next){
+	    res.send('Hello from article see');
+	});
+
+	this.expressServer.get('/article/edit/:data', function(req,res,next){
+	    res.send('Hello from article edit');
+	});
+
+	this.expressServer.get('/article/list/', function(req,res,next){
+	    res.send('Hello from article list');
+	});
+};
+
+module.exports = ExpressServer;
